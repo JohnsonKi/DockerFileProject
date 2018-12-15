463e9e59239ecf0a05c6875152ab258d81489696
jxm930 <jxm930@gmail.com>
2018-12-15 18:32:50 +0900

    fix

--- laravel_webbase/conf/entrypoint.sh
+++ laravel_webbase/conf/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ -e /usr/src/myapp ]]; then
     laravel new myapp
 else
     cd /usr/src
-    git clone https://github.com/JohnsonKi/laravelhome.git myapp && cd /usr/src/myapp && composer install
+    git clone https://github.com/JohnsonKi/laravelhome.git myapp && cd /usr/src/myapp && composer install && cp .env.example .env && php artisan key:generate
 fi
 
 chown -R nginx:nginx /usr/src/myapp
